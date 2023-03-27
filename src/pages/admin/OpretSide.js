db3f501ab0a27bff3d40ee3543f167b1ce62d77f
Marius <mari96j6@stud.kea.dk>
2023-03-27 09:38:16 +0200

    mariusewers

--- src/pages/admin/OpretSide.js
+++ src/pages/admin/OpretSide.js
@@ -1,38 +1,34 @@
 import { Navigate, useNavigate } from "react-router-dom";
-import PostForm from '../../components/admin/PostForm';
+import PostForm from "../../components/admin/PostForm";
 
-export default function OpretSide(){
-const navigate = useNavigate();
+export default function OpretSide() {
+  const navigate = useNavigate();
 
-
-async function createPost(newPost){
-
-    const url ="https://fido-og-fidoline-default-rtdb.europe-west1.firebasedatabase.app/produkter.json";
-    const   firstResponse = await fetch(url);
+  async function createPost(newPost) {
+    const url =
+      "https://fido-og-fidoline-default-rtdb.europe-west1.firebasedatabase.app/produkter.json";
+    const firstResponse = await fetch(url);
     let firstData = await firstResponse.json();
 
-    if (firstData === null){
-        firstData =[]
+    if (firstData === null) {
+      firstData = [];
     }
 
     firstData.push(newPost);
 
-    const response = await fetch(url, 
-        {method:"PUT",
-        body: JSON.stringify(firstData)
-});
-const data = await response.json();
-console.log(data);
-navigate("/admin");
-}
+    const response = await fetch(url, {
+      method: "PUT",
+      body: JSON.stringify(firstData),
+    });
+    const data = await response.json();
+    console.log(data);
+    navigate("/admin");
+  }
 
-return(
-<section className="page">
-    <h1>Opret nyt produkt</h1>
-    <PostForm savePost={createPost}/>
+  return (
+    <section className="page">
+      <h1>Opret nyt produkt</h1>
+      <PostForm savePost={createPost} />
     </section>
-    )
-    
-
+  );
 }
-
